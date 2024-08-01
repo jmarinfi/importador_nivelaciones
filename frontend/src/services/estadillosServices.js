d28de5086a14333974e968ac8aa176d5362239b5
Joan <jmarinfi@uoc.edu>
2024-08-01 13:37:48 -0500

    Se modifica la obtención de la url base

--- frontend/src/services/estadillosServices.js
+++ frontend/src/services/estadillosServices.js
@@ -1,8 +1,9 @@
-const base_url_dev = 'http://localhost:3005/api'
-const base_url_prod = '/api'
+const base_url = process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3005/api'
+    : '/api'
 
 export const getListasEstadillos = async () => {
-    const response = await fetch(`${base_url_dev}/listas`)
+    const response = await fetch(`${base_url}/listas`)
     if (!response.ok) {
         throw new Error('Se ha producido un error al obtener las listas')
     }
@@ -10,7 +11,7 @@ export const getListasEstadillos = async () => {
 }
 
 export const getSensoresLista = async (idLista) => {
-    const response = await fetch(`${base_url_dev}/sensores-lista/${idLista}`)
+    const response = await fetch(`${base_url}/sensores-lista/${idLista}`)
     if (!response.ok) {
         throw new Error('Error al obtener los sensores de la lista')
     }
@@ -18,7 +19,7 @@ export const getSensoresLista = async (idLista) => {
 }
 
 export const getNombreLista = async (idLista) => {
-    const response = await fetch(`${base_url_dev}/nombre-lista/${idLista}`)
+    const response = await fetch(`${base_url}/nombre-lista/${idLista}`)
     if (!response.ok) {
         throw new Error('Error al obtener el nombre de la lista')
     }
